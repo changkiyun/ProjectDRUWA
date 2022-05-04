5008727d4691d36839bf1567e2145a684c8b001a
changki yun <ekzm7845@naver.com>
2022-05-04 16:35:35 +0900

    branch test2

--- app/src/main/java/com/example/projectdruwa/MainActivity.java
+++ app/src/main/java/com/example/projectdruwa/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
     //branch Test
+    //branch Test2
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
