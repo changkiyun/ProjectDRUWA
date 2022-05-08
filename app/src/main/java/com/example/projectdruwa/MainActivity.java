a266b9d0210587b00b2a3f9d44518aa01c062918
luglee <yele3030@gmail.com>
2022-05-08 22:30:19 +0900

    TODO Spinner클래스 구현 해야함

--- app/src/main/java/com/example/projectdruwa/MainActivity.java
+++ app/src/main/java/com/example/projectdruwa/MainActivity.java
@@ -15,5 +15,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // TODO: 2022-05-08 spinner 클래스 구현 http://dailyddubby.blogspot.com/2018/03/11-spinnercombobox-custom.html
     }
 }
