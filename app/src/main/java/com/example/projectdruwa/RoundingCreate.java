af4dc2a2b1c2a59340f86f3cd6b6239b3cdafdc7
luk8121 <yele3030@gmail.com>
2022-05-12 20:05:08 +0900

    ChoiJiWoong

--- app/src/main/java/com/example/projectdruwa/RoundingCreate.java
+++ app/src/main/java/com/example/projectdruwa/RoundingCreate.java
@@ -15,7 +15,7 @@ public class RoundingCreate extends Activity {
         setContentView(R.layout.rounding_create);
         setTitle("라운딩 매칭");
 
-        //todo 2 : value값에 spinner 구현 추가
+        //todo 2 : value값에 spinner 구현 추가 필요
         //todo 1 : finish로 끝낼것이 아니라 위에서 선택한 정보들을 DB로넘겨주며 방을 생성하는 기능 추가 필요
         Button createRoom = findViewById(R.id.createRoom);
         createRoom.setOnClickListener(new View.OnClickListener() {
