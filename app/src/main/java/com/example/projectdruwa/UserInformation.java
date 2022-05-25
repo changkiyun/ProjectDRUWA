b0adebc5ba49b3a06e23d2acf36f8f9fbb1426ab
GOodDay0000000 <104884317+GOodDay0000000@users.noreply.github.com>
2022-05-25 14:26:03 +0900

    JMS

--- app/src/main/java/com/example/projectdruwa/UserInformation.java
+++ app/src/main/java/com/example/projectdruwa/UserInformation.java
@@ -14,6 +14,7 @@ import androidx.fragment.app.Fragment;
 //user_information Fragment 처리 YCK
 public class UserInformation extends Fragment {
 
+    Button btn_edit;
 
     @Nullable
     @Override
@@ -31,5 +32,16 @@ public class UserInformation extends Fragment {
 
     private void initUI(ViewGroup rootView){
 
+
+            btn_edit = rootView.findViewById(R.id.btn_edit);
+            btn_edit.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Intent intent = new Intent(getContext(), ProfileEdit.class);
+                    startActivity(intent);
+                }
+                });
+        }
     }
-}
+
+
